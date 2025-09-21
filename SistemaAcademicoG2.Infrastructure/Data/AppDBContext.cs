c35a05fff944d3acf688aa1f8c3ad8188fb9d7c5
victor gutierrez <hola@victorgutierrez.com>
2025-09-20 22:52:15 -0600

    Agregar archivos de proyecto.

---
+++ SistemaAcademicoG2.Infrastructure/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademicoG2.Infrastructure.Data
+{
+    internal class AppDBContext
+    {
+    }
+}
