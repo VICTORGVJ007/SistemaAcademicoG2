122500a75a7ee24415fbf1d70099e0bdf25e8cb4
victor gutierrez <hola@victorgutierrez.com>
2025-10-02 11:03:17 -0600

    realize migracion

---
+++ SistemaAcademicoG2.Infrastructure/Migrations/20251002170025_InitialCreate.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaAcademicoG2.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_Asignatura",
+                columns: table => new
+                {
+                    idasignatura = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    estado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Asignatura", x => x.idasignatura);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_Asistencia",
+                columns: table => new
+                {
+                    AsistenciaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Grado = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Fecha = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Estado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Asistencia", x => x.AsistenciaId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_Grado",
+                columns: table => new
+                {
+                    idGrado = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    estado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Grado", x => x.idGrado);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_Inscripcion",
+                columns: table => new
+                {
+                    idInscripcion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false),
+                    idGrado = table.Column<int>(type: "int", nullable: false),
+                    fechaIngreso = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    estado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Inscripcion", x => x.idInscripcion);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_Nota",
+                columns: table => new
+                {
+                    idnota = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    idusuario = table.Column<int>(type: "int", nullable: false),
+                    idasignatura = table.Column<int>(type: "int", nullable: false),
+                    periodo = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    nota = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    estadoacademico = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    estado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Nota", x => x.idnota);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_Rol",
+                columns: table => new
+                {
+                    RolId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Estado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Rol", x => x.RolId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_Usuario",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdRol = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Apellido = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Correo = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Clave = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Estado = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_Usuario", x => x.IdUsuario);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "t_grado_asignatura",
+                columns: table => new
+                {
+                    IdGradoAsignatura = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdGrado = table.Column<int>(type: "int", nullable: false),
+                    GradoIdGrado = table.Column<int>(type: "int", nullable: false),
+                    IdAsignatura = table.Column<int>(type: "int", nullable: false),
+                    AsignaturaId = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_grado_asignatura", x => x.IdGradoAsignatura);
+                    table.ForeignKey(
+                        name: "FK_t_grado_asignatura_t_Asignatura_AsignaturaId",
+                        column: x => x.AsignaturaId,
+                        principalTable: "t_Asignatura",
+                        principalColumn: "idasignatura",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_t_grado_asignatura_t_Grado_GradoIdGrado",
+                        column: x => x.GradoIdGrado,
+                        principalTable: "t_Grado",
+                        principalColumn: "idGrado",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_grado_asignatura_AsignaturaId",
+                table: "t_grado_asignatura",
+                column: "AsignaturaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_grado_asignatura_GradoIdGrado",
+                table: "t_grado_asignatura",
+                column: "GradoIdGrado");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_Asistencia");
+
+            migrationBuilder.DropTable(
+                name: "t_grado_asignatura");
+
+            migrationBuilder.DropTable(
+                name: "t_Inscripcion");
+
+            migrationBuilder.DropTable(
+                name: "t_Nota");
+
+            migrationBuilder.DropTable(
+                name: "t_Rol");
+
+            migrationBuilder.DropTable(
+                name: "t_Usuario");
+
+            migrationBuilder.DropTable(
+                name: "t_Asignatura");
+
+            migrationBuilder.DropTable(
+                name: "t_Grado");
+        }
+    }
+}
